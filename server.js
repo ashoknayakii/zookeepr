eb9f9f65fb6a748720d2f9e89307f0af4e18e819
Ashok Nayak <ashok.nayakii@gmail.com>
2022-03-03 19:02:01 -0500

    public folders created and linked

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
